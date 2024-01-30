af964ca24253318ef9a0063b7871a7d45c7c0d80
KrushnaKale07 <krushnakale0007@gmail.com>
2024-01-30 14:11:14 +0530

    first Commit

---
+++ CrudApllication/src/main/java/com/example/CrudApllication/CrudApllicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrudApllication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApllicationApplication.class, args);
+	}
+
+}
---
+++ CrudApllication/src/test/java/com/example/CrudApllication/CrudApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudApllication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
